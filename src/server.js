52215a735c77de9f686a218706c9323a4a283b9a
Breno Farias Sousa <brenosousa.farias@outlook.com>
2020-03-15 19:49:29 -0300

    added CORS
    CORS has been added so that the api can be accessed by other applications.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const routes = require("./routes");
 
 const server = express();
@@ -9,6 +11,7 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
