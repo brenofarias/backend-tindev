e3ecd923d3f985df987c284f3bee548d29ebcfe6
Breno Farias Sousa <brenosousa.farias@outlook.com>
2020-03-15 18:53:00 -0300

    integration with the github api
    dev schema and controller were created and the github api was used to obtain dev information

---
+++ src/controllers/DevController.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async store(req, res) {
+    const { username } = req.body;
+
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
+
+    const { name, bio, avatar_url: avatar } = response.data;
+
+    const dev = await Dev.create({
+      name: name,
+      user: username,
+      bio: bio,
+      avatar: avatar
+    });
+
+    return res.json(dev);
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,8 @@
 const express = require("express");
+const DevContrroler = require("./controllers/DevController");
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: `Olá ${req.query.name}` });
-});
-
-routes.post("/devs", (req, res) => {
-  return res.json(req.body);
-});
+routes.post("/devs", DevContrroler.store);
 
 module.exports = routes;
